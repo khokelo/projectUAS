086a55394ad60281b0dffbdc67375b98e270fd2a
U S E R <U S E R@DESKTOP-VUG4U49>
2024-01-04 06:59:44 +0700

    27 5

---
+++ src/bismillah/Bismillah.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bismillah;
+
+import aplikasi1.home;
+
+/**
+ *
+ * @author U S E R
+ */
+public class Bismillah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        home k = new home();
+        k.setLocationRelativeTo(null);
+        k.setVisible(true);
+    }
+    
+}
